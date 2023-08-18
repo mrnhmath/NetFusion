2582fa9f498c72b368ff759a743c4fa85b6393e2
Matheus Marinho <matheus4551@hotmail.com>
2023-08-18 12:59:21 -0300

    Remove Tracking Protection support

--- suite/common/pref/pref-security.js
+++ suite/common/pref/pref-security.js
@@ -3,12 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-function Startup()
-{
-  var prefTrackProtect = document.getElementById("privacy.trackingprotection.enabled");
-  SetWarnTrackEnabled(prefTrackProtect.value);
-}
-
 function SetWarnTrackEnabled(aEnable)
 {
   EnableElementById("warnTrackContent", aEnable, false);
